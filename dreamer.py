3c82330fc244915f4eb9ca2d99bbaf08a9d5a976
Inexperienced Me <inexperiencedme@protonmail.com>
2024-10-29 00:59:27 +0100

    big changes, doesnt seem to work

--- dreamer.py
+++ dreamer.py
@@ -2,33 +2,91 @@ import torch
 import torch.nn as nn
 import torch.functional as F
 from utils import *
+from neuralNets import *
 
 class Dreamer:
     def __init__(self):
-        pass
+        self.representationClasses = 16
+        self.representationSize = self.representationClasses ** 2
+        self.actionSize = 3
+        self.recurrentStateSize = 256
+        self.compressedObservationsSize = 256
+        self.obsShape = (3, 96, 96)
 
-    def train():
-        pass
-        # Encode all obs with encoder that uses symlog and outputs recurrentStateSize-sized output
+        self.convEncoder     = ConvEncoder(self.obsShape, self.compressedObservationsSize).to(device)
+        self.convDecoder     = ConvDecoder(self.representationSize + self.recurrentStateSize, self.obsShape).to(device)
+        self.sequenceModel   = SequenceModel(self.representationSize, self.actionSize, self.recurrentStateSize).to(device)
+        self.priorNet        = PriorNet(self.recurrentStateSize, self.representationClasses).to(device)
+        self.posteriorNet    = PosteriorNet(self.recurrentStateSize + self.compressedObservationsSize, self.representationClasses).to(device)
+        self.rewardPredictor = RewardPredictor(self.recurrentStateSize + self.representationSize).to(device)
 
-        # Init recurrent state
-        # Going through the whole episode:
-            # pass recurrent state and transformed obs to posterior net
-            # append posterior net output to a list
+        self.worldModelOptimizer = optim.AdamW(
+            list(self.convEncoder.parameters()) + list(self.convDecoder.parameters()) + 
+            list(self.sequenceModel.parameters()) + list(self.priorNet.parameters()) + 
+            list(self.posteriorNet.parameters()) + list(self.rewardPredictor.parameters()), 
+            lr=1e-3
+)
 
-            # _, recurrentState = rnn(posteriorNetOutput + action)
-            # append recurrentState to a list of recurrentStates
+    def train(self, observations, actions, rewards):
+        encodedObservations = self.convEncoder(observations)
+        initialRecurrentState = self.sequenceModel.initializeRecurrentState().to(device)
+        episodeLength = len(actions)
 
-            # pass recurrentState to a priorNet to get priorNetOutput
-            # append priorNetOutput to a list
+        posteriorNetOutputs = []
+        recurrentStates = [initialRecurrentState]
+        priorNetLogits = []
+        posteriorNetLogits = []
 
-        # stack tensors
+        for timestep in range(episodeLength):
+            posteriorNetOutput, posteriorNetCurrentLogits = self.posteriorNet(torch.cat((recurrentStates[timestep], encodedObservations[timestep]), -1))
+            posteriorNetOutputs.append(posteriorNetOutput)
+            posteriorNetLogits.append(posteriorNetCurrentLogits)
 
-        # kl loss computation
+            recurrentState = self.sequenceModel(posteriorNetOutputs[timestep].detach(), actions[timestep], recurrentStates[timestep])
+            recurrentStates.append(recurrentState)
 
-        # calculate fullStateRepresentation by concatenating recurrentState and posteriorNetOutput
-        # pass through a decoder fullStateRepresentation to get back obs
+            _, priorNetCurrentLogits = self.priorNet(recurrentStates[timestep])
+            priorNetLogits.append(priorNetCurrentLogits)
 
-        # in the future will be passing fullStateRepresentation to get also rewards and continuation
+        posteriorNetOutputs = torch.stack(posteriorNetOutputs)      # [episodeLength    , representationSize]
+        recurrentStates = torch.stack(recurrentStates)              # [episodeLength + 1, recurrentStateSize]
+        priorNetLogits = torch.stack(priorNetLogits)                # [episodeLength    , representationSize]
+        posteriorNetLogits = torch.stack(posteriorNetLogits)        # [episodeLength    , representationSize]
+        fullStateRepresentations = torch.cat((recurrentStates[1:], posteriorNetOutputs), -1)
 
-        # calculate all losses and step the optimizer
+        reconstructedObservations = self.convDecoder(fullStateRepresentations)
+        predictedRewards = self.rewardPredictor(fullStateRepresentations)
+
+        reconstructionLoss = F.mse_loss(reconstructedObservations, observations[1:], reduction="none").mean(dim=[-1, -2, -3]).mean()
+        priorNetLoss = F.mse_loss(priorNetLogits, posteriorNetLogits.detach())
+        rewardPredictorLoss = 0.05*F.mse_loss(predictedRewards, rewards)
+
+        worldModelLoss = reconstructionLoss + priorNetLoss + rewardPredictorLoss
+
+        self.worldModelOptimizer.zero_grad()
+        worldModelLoss.backward()
+        self.worldModelOptimizer.step()
+
+        return worldModelLoss.item(), reconstructionLoss.item(), priorNetLoss.item(), rewardPredictorLoss.item()
+    
+    def reconstructObservations(self, observations, actions):
+        encodedObservations = self.convEncoder(observations)
+        initialRecurrentState = self.sequenceModel.initializeRecurrentState().to(device)
+        episodeLength = len(actions)
+
+        posteriorNetOutputs = []
+        recurrentStates = [initialRecurrentState]
+
+        for timestep in range(episodeLength):
+            posteriorNetOutput, _ = self.posteriorNet(torch.cat((recurrentStates[timestep], encodedObservations[timestep]), -1))
+            posteriorNetOutputs.append(posteriorNetOutput)
+
+            recurrentState = self.sequenceModel(posteriorNetOutputs[timestep].detach(), actions[timestep], recurrentStates[timestep])
+            recurrentStates.append(recurrentState)
+
+        posteriorNetOutputs = torch.stack(posteriorNetOutputs)      # [episodeLength    , representationSize]
+        recurrentStates = torch.stack(recurrentStates)              # [episodeLength + 1, recurrentStateSize]
+        fullStateRepresentations = torch.cat((recurrentStates[1:], posteriorNetOutputs), -1)
+        reconstructedObservations = self.convDecoder(fullStateRepresentations)
+
+        return reconstructedObservations.detach()
---
+++ neuralNets.py
@@ -0,0 +1,100 @@
+import torch
+import torch.nn as nn
+import torch.functional as F
+from utils import *
+
+class SequenceModel(nn.Module):
+    def __init__(self, representationSize, actionSize, recurrentStateSize):
+        super().__init__()
+        self.representationSize = representationSize
+        self.actionSize = actionSize
+        self.recurrentStateSize = recurrentStateSize
+        self.recurrent = nn.GRUCell(representationSize + actionSize, recurrentStateSize)
+
+    def forward(self, representation, action, recurrentState):
+        return self.recurrent(torch.cat((representation, action), -1), recurrentState)
+    
+    def initializeRecurrentState(self):
+        return torch.zeros(self.recurrentStateSize)
+
+class PriorNet(nn.Module):
+    def __init__(self, inputSize, representationClasses=16):
+        super().__init__()
+        self.representationSize = representationClasses
+        self.mlp = sequentialModel1D(inputSize, [256, 256], representationClasses**2)
+    
+    def forward(self, x):
+        logits = self.mlp(x)
+        stateMatrix = logits.view(-1, self.representationSize, self.representationSize)
+        _, indices = torch.max(stateMatrix, dim=-1)
+        representation = F.one_hot(indices, num_classes=self.representationSize)
+        return representation.view(-1), logits
+    
+class PosteriorNet(nn.Module):
+    def __init__(self, inputSize, representationClasses=16):
+        super().__init__()
+        self.representationSize = representationClasses
+        self.mlp = sequentialModel1D(inputSize, [256, 256], representationClasses**2)
+    
+    def forward(self, x):
+        logits = self.mlp(x)
+        stateMatrix = logits.view(-1, self.representationSize, self.representationSize)
+        _, indices = torch.max(stateMatrix, dim=-1)
+        representation = F.one_hot(indices, num_classes=self.representationSize)
+        return representation.view(-1), logits
+
+class ConvEncoder(nn.Module):
+    def __init__(self, inputShape, outputSize):
+        super(ConvEncoder, self).__init__()
+        c, h, w = inputShape
+        self.convolutionalNet = nn.Sequential(
+            nn.Conv2d(c, 32, kernel_size=4, stride=2, padding=1),  # Output: (32, h/2, w/2)
+            nn.Tanh(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),  # Output: (64, h/4, w/4)
+            nn.Tanh(),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # Output: (128, h/8, w/8)
+            nn.Tanh(),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),  # Output: (256, h/16, w/16)
+            nn.Tanh(),
+            nn.Flatten(),
+            nn.Linear(256 * (h // 16) * (w // 16), outputSize),
+            nn.Tanh(),
+        )
+
+    def forward(self, obs):
+        return self.convolutionalNet(obs.float())
+
+class ConvDecoder(nn.Module):
+    def __init__(self, inputSize, outputShape):
+        super(ConvDecoder, self).__init__()
+        self.outputShape = outputShape
+        c, h, w = outputShape
+        self.fc = nn.Sequential(
+            nn.Linear(inputSize, 256 * (h // 16) * (w // 16)),
+            nn.Tanh(),
+        )
+        self.deconvolutionalNet = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),  # Output: (128, h/8, w/8)
+            nn.Tanh(),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),  # Output: (64, h/4, w/4)
+            nn.Tanh(),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),  # Output: (32, h/2, w/2)
+            nn.Tanh(),
+            nn.ConvTranspose2d(32, c, kernel_size=4, stride=2, padding=1),  # Output: (c, h, w)
+            nn.Sigmoid(),  # Output pixel values between 0 and 1
+        )
+
+    def forward(self, x):
+        x = self.fc(x)
+        batchSize = x.size(0)
+        c, h, w = 256, self.outputShape[1] // 16, self.outputShape[2] // 16
+        x = x.view(batchSize, c, h, w)
+        return (self.deconvolutionalNet(x))
+
+class RewardPredictor(nn.Module):
+    def __init__(self, inputSize):
+        super(RewardPredictor, self).__init__()
+        self.mlp = sequentialModel1D(inputSize, [256, 256], 1)
+
+    def forward(self, x):
+        return self.mlp(x)
