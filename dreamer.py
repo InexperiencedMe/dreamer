new fixed actor critic loop, logprobs of action leading to state, still not quite right
Im very confused here. Lots of debugging. State evaluation randomly skyrockets, rewardPredictor outputs seemingly random values for actor critic, but completely reasonable valid values for world model training... While it used to be correct in runs like NEW_HOPE7_YES at 121k iterations. Very weird

dreamer.py
@@ -28,8 +28,8 @@ class Dreamer:
         self.gamma                      = 0.997
         self.lambda_                    = 0.95
         self.worldModelLR               = 1e-4
-        self.criticLR                   = 8e-5
-        self.actorLR                    = 8e-5
+        self.criticLR                   = 1e-4
+        self.actorLR                    = 5e-5
         
         self.convEncoder     = ConvEncoder(self.obsShape, self.compressedObservationSize).to(device)
         self.convDecoder     = ConvDecoder(self.representationSize + self.recurrentStateSize, self.obsShape).to(device)
@@ -134,10 +134,10 @@ class Dreamer:
 
     def trainActorCritic(self, initialFullState):
         fullState = initialFullState.detach()                       # [actorCriticBatchSize, recurrentSize + representationSize]
-        action = self.actor(fullState, training=False)              # [actorCriticBatchSize, actionSize]
+        action, logProbabilities, entropy = self.actor(fullState.detach())
         recurrentState, latentState = torch.split(fullState, [self.recurrentStateSize, self.representationSize], -1)
         
-        fullStates, actionLogProbabilities, entropies = [], [], []
+        fullStates, actionLogProbabilities, entropies = [fullState], [logProbabilities], [entropy]
         for _ in range(self.imaginationHorizon):
             recurrentState = self.sequenceModel(latentState, action, recurrentState)
             latentState, _ = self.priorNet(recurrentState)
@@ -148,19 +148,20 @@ class Dreamer:
             actionLogProbabilities.append(logProbabilities)
             entropies.append(entropy)
 
-        fullStates              = torch.stack(fullStates, dim=1)                            # [batchSize, horizon, recurrentSize + representationSize]
-        actionLogProbabilities  = torch.stack(actionLogProbabilities[:-1], dim=1)           # [batchSize, horizon-1]
-        entropies               = torch.stack(entropies[:-1], dim=1)                        # [batchSize, horizon-1]
+        fullStates              = torch.stack(fullStates[1:], dim=1)                            # [batchSize, horizon, recurrentSize + representationSize]
+        actionLogProbabilities  = torch.stack(actionLogProbabilities[:-2], dim=1)               # [batchSize, horizon-1]
+        entropies               = torch.stack(entropies[:-2], dim=1)                            # [batchSize, horizon-1]
 
         with torch.no_grad():
-            predictedRewards    = self.rewardPredictor(fullStates[:, :-1], useSymexp=True)                                                  # [batchSize, horizon-1]
             targetCriticValues  = self.targetCritic(fullStates)                                                                             # [batchSize, horizon]
+            predictedRewards    = self.rewardPredictor(fullStates[:, :-1], useSymexp=True)                                                  # [batchSize, horizon-1]
             targetLambdaValues  = self.lambdaValues(predictedRewards, targetCriticValues, gamma=self.gamma, lambda_=self.lambda_)           # [batchSize, horizon-1]
             _, inverseScale     = self.valueMoments(targetLambdaValues)
             advantages          = (targetLambdaValues - targetCriticValues[:, :-1])/inverseScale
 
         # Actor Update
         # NOTE: Actor has to use .sample() when using advantages, .rsample() when using -lambdaValues
+        # NOTE: Official paper has a mistake here. Of course we take advantage of this state and logprob of the previous action, not this action
         actorLoss = -torch.mean(advantages.detach()*actionLogProbabilities + self.entropyScale*entropies)
         # actorLoss = -torch.mean(lambdaValues) # DreamerV1 style loss
 
@@ -171,9 +172,9 @@ class Dreamer:
         self.actorOptimizer.step()
 
         # Critic Update
-        criticValues = self.critic(fullStates[:, :-1].detach())
+        criticValues = self.critic(fullStates.detach())
         lambdaValues = self.lambdaValues(predictedRewards, criticValues, gamma=self.gamma, lambda_=self.lambda_)
-        criticLoss = F.mse_loss(criticValues, lambdaValues.detach())
+        criticLoss = F.mse_loss(criticValues[:, :-1], lambdaValues.detach())
 
         self.criticOptimizer.zero_grad()
         criticLoss.backward()
@@ -182,7 +183,7 @@ class Dreamer:
         self.criticOptimizer.step()
 
         for param, targetParam in zip(self.critic.parameters(), self.targetCritic.parameters()):
-            targetParam.data.copy_(self.tau * param.data + (1 - self.tau) * targetParam.data)
+            targetParam.data.copy_(self.tau*param.data + (1 - self.tau)*targetParam.data)
 
         return criticLoss.cpu().item(), actorLoss.cpu().item(), targetCriticValues.mean().cpu().item(), criticValues.mean().cpu().item()
 

main.py
@@ -9,22 +9,22 @@ np.set_printoptions(threshold=2000, linewidth=200)
 
 environmentName         = "CarRacing-v3"
 renderMode              = None
-seed                    = 2
+seed                    = 1
 stepCountLimit          = 256                   # Determines sequenceLength for now
 
-episodesBeforeStart     = 40
+episodesBeforeStart     = 10
 numNewEpisodePlay       = 1
 playInterval            = 10
 saveMetricsInterval     = 10
 checkpointInterval      = 1000
 bufferSize              = 40                    # 50 throws me an out of memory error (it's all on gpu), increase if possible
 
-numUpdates              = 36000
+numUpdates              = 60000
 resume                  = True
 saveMetrics             = True
 saveCheckpoints         = True
-runName                 = f"{environmentName}_NEW_HOPE4"
-checkpointToLoad        = f"checkpoints/{runName}_114000"
+runName                 = f"{environmentName}_NEW_RUN"
+checkpointToLoad        = f"checkpoints/{runName}_40000"
 metricsFilename         = f"metrics/{runName}"
 plotFilename            = f"plots/{runName}"
 videoFilename           = f"videos/{runName}"

neuralNets.py
@@ -5,7 +5,6 @@ from utils import *
 import torch.distributions as distributions
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
 class SequenceModel(nn.Module):
     def __init__(self, representationSize, actionSize, recurrentStateSize):
         super().__init__()
@@ -27,7 +26,7 @@ class PriorNet(nn.Module):
         self.representationLength = representationLength
         self.representationClasses = representationClasses
         self.representationSize = representationLength*representationClasses
-        self.mlp = sequentialModel1D(inputSize, [512, 256], self.representationSize)
+        self.mlp = sequentialModel1D(inputSize, [512, 512], self.representationSize)
         self.uniformMix = 0.01
     
     def forward(self, x):
@@ -48,7 +47,7 @@ class PosteriorNet(nn.Module):
         self.representationLength = representationLength
         self.representationClasses = representationClasses
         self.representationSize = representationLength*representationClasses
-        self.mlp = sequentialModel1D(inputSize, [512, 256], self.representationSize)
+        self.mlp = sequentialModel1D(inputSize, [512, 512], self.representationSize)
         self.uniformMix = 0.01
     
     def forward(self, x):
@@ -116,11 +115,12 @@ class ConvDecoder(nn.Module):
 class RewardPredictor(nn.Module):
     def __init__(self, inputSize):
         super(RewardPredictor, self).__init__()
-        self.mlp = sequentialModel1D(inputSize, [256, 256], 1)
+        self.mlp = sequentialModel1D(inputSize, [512, 512], 1)
         self.setLastLayerToZeros(self.mlp)
 
     def forward(self, x, useSymexp=False):
         out = self.mlp(x).squeeze(-1)
+        # print(f"in RewardPredictor:\ninput first batch last element: {x[0][-1]},\noutput:{out}")
         if useSymexp:
             return symexp(out)
         else:
@@ -136,8 +136,8 @@ LOG_STD_MIN = -5
 class Actor(nn.Module):
     def __init__(self, inputSize, actionSize, actionLow=[-1], actionHigh=[1]):
         super(Actor, self).__init__()
-        self.mean = sequentialModel1D(inputSize, [256, 256], actionSize)
-        self.logStd = sequentialModel1D(inputSize, [256, 256], actionSize)
+        self.mean = sequentialModel1D(inputSize, [512, 256], actionSize)
+        self.logStd = sequentialModel1D(inputSize, [512, 256], actionSize)
         self.register_buffer("actionScale", ((torch.tensor(actionHigh, device=device) - torch.tensor(actionLow, device=device)) / 2.0))
         self.register_buffer("actionBias", ((torch.tensor(actionHigh, device=device) + torch.tensor(actionLow, device=device)) / 2.0))
 
@@ -155,7 +155,7 @@ class Actor(nn.Module):
         if training:
             logProbabilities = distribution.log_prob(sample)
             logProbabilities -= torch.log(self.actionScale * (1 - sampleTanh.pow(2)) + 1e-6)
-            return action, logProbabilities.sum(-1), distribution.entropy().sum(-1)
+            return action, logProbabilities.sum(-1), distribution.entropy().sum(-1) # I really think this should be mean, but SheepRL has sum
         else:
             return action
 
@@ -163,7 +163,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     def __init__(self, inputSize):
         super(Critic, self).__init__()
-        self.mlp = sequentialModel1D(inputSize, [256, 256], 1)
+        self.mlp = sequentialModel1D(inputSize, [512, 512], 1)
         self.setLastLayerToZeros(self.mlp)
 
     def forward(self, x):